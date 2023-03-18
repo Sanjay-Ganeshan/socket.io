9d67f3a57eb86b997acc0ff13ff25d667e6ace6d
Sanjay Ganeshan <sangan@live.com>
2023-03-18 02:33:18 -0400

    Update index.js
    CORS

--- examples/whiteboard/index.js
+++ examples/whiteboard/index.js
@@ -1,8 +1,13 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*'
+  },
+  transports: ['websocket']
+});
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
